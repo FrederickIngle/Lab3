6afbd5f5b80f1665bd5cbe9f797a1d41f6d83337
FrederickIngle <fingle2016@fau.edu>
2024-02-10 04:30:50 -0500

    css

--- barista-app/src/App.jsx
+++ barista-app/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div>
       <div className="title-container">
+        <img src="https://quiet-macaron-ca013a.netlify.app/assets/omg-logo.79cdfddd.png" alt="coffee" className="coffee-icon"/>
         <h1 className='title'>On My Grind</h1>
         <p>So you think you can barista? Let's put that to the test...</p>
       </div>
